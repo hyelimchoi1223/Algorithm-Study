3144636b4b2e4c5c72aaef1f5191451952af3010
hyelimchoi1223 <hlchoi1223@gmail.com>
2021-02-16 15:41:33 +0900

    [Edit] input 시 바로 형변환

--- [백준]2588 곱셈/python.py
+++ [백준]2588 곱셈/python.py
@@ -1,10 +1,10 @@
-value1=input()
+value1=int(input())
 value2=input()
 
 result = 0
 index = 0
 for _ in value2[::-1]:    
-    temp = int(value1)*int(_)
+    temp = value1*int(_)
     print(temp)
     result += temp*(10**index)
     index += 1
