a9d0ade06a4abcefaec227f3d1daa66bcd13fffa
hyelimchoi1223 <hlchoi1223@gmail.com>
2021-02-18 09:30:18 +0900

    [Edit] 2562번 파이썬 코드 수정

--- [백준]2562 최대값/python.py
+++ [백준]2562 최대값/python.py
@@ -1,19 +1,8 @@
-def algorithm(array):
-    temp = array.copy()
-    temp.sort()
-    print("temp:",temp)
-    print("array:",array)
-    max =temp[temp.__len__()-1]
-    print(max)
-    print(array.index(max)+1)
-    print("----")
-# array=[]
-# for _ in range(1, 10):
-#     array.append(int(input()))
-import random
-for _ in range(1, 100):
-    testData=[random.randrange(1,100) for _ in range(10)]
-    print(testData)
-    algorithm(testData)
-
-
+array=[]
+for _ in range(1, 10):
+    array.append(int(input()))
+temp = array.copy()
+temp.sort()
+max =temp[temp.__len__()-1]
+print(max)
+print(array.index(max)+1)
