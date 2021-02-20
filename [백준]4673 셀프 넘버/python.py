4114df0e3094299d27c92942b377792abfd9ef88
Hyelim <hlchoi1223@gmail.com>
2021-02-20 22:14:06 +0900

    [Edit] 백준 : 4344 파이썬 풀이 수정 속도 향상 및 방식 변경 기존 방법보다 3배정도의 실행시간을 줄임

--- [백준]4673 셀프 넘버/python.py
+++ [백준]4673 셀프 넘버/python.py
@@ -1,20 +1,21 @@
 import time
 num_list = list(range(1, 10001))
-array = []
+my_array = []
 
 
 def d(n):
+    global my_array
     my_sum = n
     my_sum += sum(list(map(int, str(n))))
     if my_sum > 10000:
         return
-    if num_list.__contains__(my_sum):
-        num_list.remove(my_sum)
+    my_array.append(my_sum)
     d(my_sum)
 
 
-start = time.time()
 for _ in num_list:
     d(_)
-    print(_)
-print(time.time()-start)
+
+my_array = list(set(my_array))
+result = [a for a in num_list if a not in my_array]
+print("\n".join(map(str, result)))
