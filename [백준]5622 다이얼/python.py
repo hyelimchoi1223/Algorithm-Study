ab06abc522a1495032cddb3306ade9ec3eb80e5c
hyelimchoi1223 <hlchoi1223@gmail.com>
2021-02-24 17:02:09 +0900

    [Edit] 백준 : 5622 python 풀이 수정

--- [백준]5622 다이얼/python.py
+++ [백준]5622 다이얼/python.py
@@ -1,4 +1,3 @@
-dial = [-1,-1,1,('A','B','C'),('D','E','F'),('G','H','I'),('J','K','L'),('M','N','O'),('P','Q','R','S'),('T','U','V'),('W','X','Y','Z'),0]
 dial_dic = {1:"", 2:"ABC", 3:"DEF", 4:"GHI", 5:"JKL", 6:"MNO", 7:"PQRS", 8:"TUV", 9:"WXYZ", 0:""}
 value = input()
 result = 0
