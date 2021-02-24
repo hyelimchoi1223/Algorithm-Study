5640866f72b5ba04925650f74ecc1da83508614e
hyelimchoi1223 <hlchoi1223@gmail.com>
2021-02-24 17:01:40 +0900

    [Add] 백준 : 1316 문제 및 파이썬 풀이 추가

---
+++ [백준]1316  그룹 단어 체커/python.py
@@ -0,0 +1,13 @@
+count = int(input())
+word = ""
+for _ in range(3):
+    word += input()
+my_tuple = []
+firstchar = word[0]
+charCnt = 0
+for i in range(0, word.__len__()+1):
+    if firstchar == _:
+        charCnt += 1
+    else:
+        
+            
