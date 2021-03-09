6d775ff961fd01d9afa21d1eecf1fae339edcec0
hyelimchoi1223 <hlchoi1223@gmail.com>
2021-03-09 09:47:13 +0900

    [Add] 백준 : 1316 파이썬 풀이 수정 문제 해결

--- [백준]1316  그룹 단어 체커/python.py
+++ [백준]1316  그룹 단어 체커/python.py
@@ -1,17 +1,34 @@
-count = int(input())
-word = ""
-for _ in range(3):
-    word += input()
-my_tuple = []
-firstchar = word[0]
-charCnt = 1
+from itertools import groupby
+def checkgroup(word):
+    group = [key for key, item in groupby(word)]
+    values =[(k, [i for i in range(len(word)) if word[i] == k]) for k in group]
 
-index = 0
-for i in word:
-    print("1."+i)
-    index += 1
-    for j in word[index::]:
-        print(j)
+    groupword = 0
+    for items in values:
+        if items[1].__len__() == 0:
+            groupword += 1
+            continue        
+        for index in range(1, items[1].__len__()):
+            if items[1][index]-items[1][index-1] >1:
+                groupword = 0
+                return groupword
+        groupword += 1
+    return groupword    
+        
+        
+        
+count = int(input())
+words = [input() for _ in range(count)]
+result = 0
+for _ in words:
+    groupcount = checkgroup(_)
+    if groupcount != 0:
+        result+=1
+    
+print(result)
+        
+    
+
         
         
     
